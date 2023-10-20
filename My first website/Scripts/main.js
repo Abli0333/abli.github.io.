d8acbd61c464527ccda5547a5d5e688ae868f92a
Abli0333 <132413168+Abli0333@users.noreply.github.com>
2023-10-20 03:59:23 +0300

    Add files via upload

---
+++ My first website/Scripts/main.js
@@ -0,0 +1,33 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Dogs";
+const myimage = document.querySelector("img");
+myimage.onclick = function(){
+    const mysource = myimage.getAttribute("src");
+    if(mysource==="images/Dog.jpeg"){
+        myimage.setAttribute("src","images/dog2.jpeg");
+    }
+    else{
+        myimage.setAttribute("src","images/Dog.jpeg");
+    }
+    }
+    let myheading = document.querySelector("h1");
+    let mybutton = document.querySelector("button");
+    function setUserName(){
+        const username = prompt("Enter your name");
+    if (!username) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", username);
+      myheading.textContent = `Who loves Dogs, ${username}`;
+    }
+    }
+if(!localStorage.getItem("name")){
+    setUserName();
+}else{
+    const stored = localStorage.getItem("name");
+    myheading.textContent= `who loves Dogs, ${stored}`;
+}
+mybutton.onclick = function(){
+setUserName();
+};
+  
